b0b27f85b253b754799b5ffcbc8b6a651fb2402b
SRIRAMNATH M <msriramnath3@gmail.com>
2025-12-16 15:34:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function press(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    let expression = document.getElementById("display").value;
+    try {
+        document.getElementById("display").value = eval(expression);
+    } catch {
+        alert("Invalid expression");
+    }
+}
